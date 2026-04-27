356e5bdab7489731c24fa29e31eb5f53ea4d099d
udit777 <udit.sahoo@autoomstudio.com>
2026-04-27 11:20:15 +0530

    fix: explicit image remote patterns for i.pravatar.cc

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ['i.pravatar.cc', 'images.unsplash.com'],
     remotePatterns: [
-      { protocol: 'https', hostname: 'i.pravatar.cc' },
-      { protocol: 'https', hostname: 'images.unsplash.com' }
-    ]
-  }
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
+
 
 
 export default nextConfig;
